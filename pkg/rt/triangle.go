dc57d6a09d749161eac6ce443a9fa15564cfd1e7
Dave Kennedy <davepkennedy@gmail.com>
2026-04-06 17:30:31 +0100

    complete to triangles

---
+++ pkg/rt/triangle.go
@@ -0,0 +1,79 @@
+package rt
+
+import (
+	"math"
+	"reflect"
+)
+
+type Triangle struct {
+	P1, P2, P3 *Tuple
+	E1, E2 *Tuple
+	Normal *Tuple
+}
+
+func NewTriangle(p1, p2, p3 *Tuple) *Shape {
+	e1 := p2.Subtract(p1)
+    e2 := p3.Subtract(p1)
+	normal := e2.Cross(e1).Normalize()
+	return NewShape(
+		&Triangle {
+			P1:p1,
+			P2:p2,
+			P3:p3,
+			E1:e1,
+			E2:e2,
+			Normal:normal,
+		})
+}
+
+func (t *Triangle) LocalIntersect(ray Ray) Intersections {
+	return Intersections{}
+}
+
+func (t *Triangle) Equal(other ShapeTrait) bool {
+	return reflect.TypeOf(t) == reflect.TypeOf(other)
+}
+
+func (t *Triangle) String() string {
+	return "triangle{}"
+}
+
+func (t *Triangle) AsCapped() *Capped {
+	return nil
+}
+
+func (t *Triangle) Bounds() *BoundingBox {
+	bounds := NewBoundingBox(t.P1, t.P1)
+    bounds.AddPoint(t.P2)
+	bounds.AddPoint(t.P3)
+    return bounds
+}
+
+func (tri *Triangle) Intersect(shape *Shape, ray *Ray) *Intersections {
+	dir_cross_e2 := ray.Direction.Cross(tri.E2)
+    det := tri.E1.Dot(dir_cross_e2)
+    if math.Abs(det) < EPSILON {
+        return NewIntersections()
+	}
+    
+	f := 1.0 / det
+    p1_to_origin := ray.Origin.Subtract(tri.P1)
+    u := f * p1_to_origin.Dot(dir_cross_e2)
+    if u < 0 || u > 1 {
+    	return NewIntersections()
+	}
+            
+    origin_cross_e1 := p1_to_origin.Cross(tri.E1)
+    v := f * ray.Direction.Dot(origin_cross_e1)
+    if v < 0 || (u+v) > 1 {
+    	return NewIntersections()
+	}
+            
+    t := f * tri.E2.Dot(origin_cross_e1)
+    return NewIntersections(
+    		NewIntersection(t, shape))
+}
+
+func (t *Triangle) LocalNormalAt(point *Tuple) (*Tuple, error) {
+	return t.Normal, nil
+}
---
+++ test/triangles_test.go
@@ -0,0 +1,124 @@
+package test
+
+import (
+	"context"
+	"fmt"
+
+	"raytracer/pkg/rt"
+
+	"github.com/cucumber/godog"
+)
+
+func InitializeTrianglesScenario(sc *godog.ScenarioContext) {
+	sc.Given(
+		`^(\w) ← triangle\((\w+), (\w+), (\w+)\)$`,
+		func(ctx context.Context, dest, name1, name2, name3 string) (context.Context, error) {
+			p1, err := getTuple(ctx, name1)
+			if err != nil { return ctx, err }
+			p2, err := getTuple(ctx, name2)
+			if err != nil { return ctx, err }
+			p3, err := getTuple(ctx, name3)
+			if err != nil { return ctx, err }
+			t := rt.NewTriangle(p1,p2,p3)
+			return setShape(ctx, dest, t), nil
+		})
+	sc.Given(
+		`^(\w) ← triangle\(point\((\-?\d+\.?\d*), (\-?\d+\.?\d*), (\-?\d+\.?\d*)\), point\((\-?\d+\.?\d*), (\-?\d+\.?\d*), (\-?\d+\.?\d*)\), point\((\-?\d+\.?\d*), (\-?\d+\.?\d*), (\-?\d+\.?\d*)\)\)$`,
+		func(ctx context.Context, dest string, x1, y1, z1, x2, y2, z2, x3, y3, z3 float64) (context.Context, error) {
+			p1 := rt.NewPoint(x1, y1, z1)
+			p2 := rt.NewPoint(x2, y2, z2)
+			p3 := rt.NewPoint(x3, y3, z3)
+			t := rt.NewTriangle(p1,p2,p3)
+			return setShape(ctx, dest, t), nil
+		})
+
+	sc.Then(
+		`^(\w).p1 = (\w+)$`,
+		func(ctx context.Context, name, pointName string) error {
+			shape, err := getShape(ctx, name)
+			if err != nil { return err }
+			point, err := getTuple(ctx, pointName)
+			if err != nil { return err }
+			if !shape.Trait.(*rt.Triangle).P1.Equal(point) {
+				return fmt.Errorf("expected %s.p1 to be %s, got %s", name, pointName, shape.Trait.(*rt.Triangle).P1)
+			}
+			return nil
+		})	
+
+	sc.Then(
+		`^(\w).p2 = (\w+)$`,
+		func(ctx context.Context, name, pointName string) error {
+			shape, err := getShape(ctx, name)
+			if err != nil { return err }
+			point, err := getTuple(ctx, pointName)
+			if err != nil { return err }
+			if !shape.Trait.(*rt.Triangle).P2.Equal(point) {
+				return fmt.Errorf("expected %s.p2 to be %s, got %s", name, pointName, shape.Trait.(*rt.Triangle).P2)
+			}
+			return nil
+		})
+
+	sc.Then(
+		`^(\w).p3 = (\w+)$`,
+		func(ctx context.Context, name, pointName string) error {
+			shape, err := getShape(ctx, name)
+			if err != nil { return err }
+			point, err := getTuple(ctx, pointName)
+			if err != nil { return err }
+			if !shape.Trait.(*rt.Triangle).P3.Equal(point) {
+				return fmt.Errorf("expected %s.p3 to be %s, got %s", name, pointName, shape.Trait.(*rt.Triangle).P3)
+			}
+			return nil
+		})
+
+	sc.Then(
+		`^(\w).e1 = vector\((\-?\d+\.?\d*), (\-?\d+\.?\d*), (\-?\d+\.?\d*)\)$`,
+		func(ctx context.Context, name string, x, y, z float64) error {
+			shape, err := getShape(ctx, name)
+			if err != nil { return err }
+			edge := rt.NewVector(x, y, z)
+			if !shape.Trait.(*rt.Triangle).E1.Equal(edge) {
+				return fmt.Errorf("expected %s.e1 to be %s, got %s", name, edge, shape.Trait.(*rt.Triangle).E1)
+			}
+			return nil
+		})
+
+	sc.Then(
+		`^(\w).e2 = vector\((\-?\d+\.?\d*), (\-?\d+\.?\d*), (\-?\d+\.?\d*)\)$`,
+		func(ctx context.Context, name string, x, y, z float64) error {
+			shape, err := getShape(ctx, name)
+			if err != nil { return err }
+			edge := rt.NewVector(x, y, z)
+			if !shape.Trait.(*rt.Triangle).E2.Equal(edge) {
+				return fmt.Errorf("expected %s.e2 to be %s, got %s", name, edge, shape.Trait.(*rt.Triangle).E2)
+			}
+			return nil
+		})
+
+	sc.Then(
+		`^(\w).normal = vector\((\-?\d+\.?\d*), (\-?\d+\.?\d*), (\-?\d+\.?\d*)\)$`,
+		func(ctx context.Context, name string, x, y, z float64) error {
+			shape, err := getShape(ctx, name)
+			if err != nil { return err }
+			normal := rt.NewVector(x, y, z)
+			if !shape.Trait.(*rt.Triangle).Normal.Equal(normal) {
+				return fmt.Errorf("expected %s.normal to be %s, got %s", name, normal, shape.Trait.(*rt.Triangle).Normal)
+			}
+			return nil
+		})
+
+	sc.Then(
+		`^(\w+) = (\w).normal$`,
+		func(ctx context.Context, dest, source string) error {
+			shape, err := getShape(ctx, source)
+			if err != nil { return err }
+			
+			normal, err := getTuple(ctx, dest)
+			if err != nil { return err }
+
+			if !shape.Trait.(*rt.Triangle).Normal.Equal(normal) {
+				return fmt.Errorf("expected %s to be %s, got %s", dest, normal, shape.Trait.(*rt.Triangle).Normal)
+			}
+			return nil
+		})
+}
